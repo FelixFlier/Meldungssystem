63de5c637c0a640257fa5c7cb6f3157b87e7eb6d
FelixFlier <felix.flier03@gmail.com>
2025-02-27 18:32:33 +0100

    Update and rename main_2.py to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# uvicorn main_2:app --reload
+# uvicorn main:app --reload
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
